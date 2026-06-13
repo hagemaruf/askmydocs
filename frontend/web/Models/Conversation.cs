6f89c1a4e57beea018918c9a44dfe7664464dae8
hagemaruf <hagemaruf@gmail.com>
2026-06-13 20:00:41 +0700

    Add multi conversation chat system

---
+++ frontend/web/Models/Conversation.cs
@@ -0,0 +1,13 @@
+﻿namespace AskMyDocs.Web.Models;
+
+public class Conversation
+{
+    public Guid Id { get; set; }
+        = Guid.NewGuid();
+
+    public string Title { get; set; }
+        = "New Chat";
+
+    public List<ChatMessage> Messages
+    { get; set; } = new();
+}
